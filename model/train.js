8e0ac4e84c26690245a3727a7d3edf4efde648c8
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2024-11-14 19:21:44 +0900

    добавил файлы model и train

---
+++ model/train.js
@@ -0,0 +1,42 @@
+const tf = require('@tensorflow/tfjs-node');
+const fs = require('fs');
+const path = require('path');
+const { createModel } = require('./model')
+
+// Функция для загрузки и подготовки данных (упрощенная)
+async function loadData(dataDir) {
+    const images = [];
+    const labels = [];
+    const labelNames = fs.readdirSync(dataDir); // Получение имен папок-классов
+
+    // Загружаем изображения
+    for (let labelIndex = 0; labelIndex < labelNames.length; labelIndex++) {
+        const label = labelNames[labelIndex];
+        const imageFiles = fs.readdirSync(path.join(dataDir, label));
+
+        for (const imageFile of imageFiles) {
+            const imgPath = path.join(dataDir, label, imageFile);
+            const image = fs.readFileSync(imgPath);
+            const tensor = tf.node.decodeImage(image).resizeBilinear([128, 128]).expandDims(0).div(255);
+            images.push(tensor);
+            labels.push(labelIndex);
+        }
+    }
+    
+    return [tf.concat(images), tf.tensor1d(labels, 'int32')]; // Объединяем тензоры
+}
+
+async function train() {
+    const model = createModel();
+    const [images, labels] = await loadData('path_to_your_data');
+
+    await model.fit(images, labels, {
+        epochs: 10,
+        callbacks: tf.callbacks.earlyStopping({monitor: 'loss'}),
+    });
+
+    // Сохранение модели
+    await model.save('file://model/my_model');
+}
+
+train().catch(console.error);
