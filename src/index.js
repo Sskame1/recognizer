cd5fff83d9129b5dbcb01f088568733b6ee6f3f4
Anatoliy <119749324+Sskame1@users.noreply.github.com>
2024-11-16 01:53:16 +0900

    что то вроде добавил

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ const fs = require('fs');
 const path = require('path');
 
 async function loadImagePath() {
-    const imagePath = __dirname + '/image/raw/image.jpg'; // Замените на фактический путь
+    const imagePath = path.join(__dirname, 'image', 'raw', 'image.jpg'); // Замените на фактический путь
     return imagePath;
 }
 
 async function sliceImageAndExtractText(imagePath, rows, cols, outputDir, customNames) {
     // Создаем папку, если она не существует
-    if (!fs.existsSync(outputDir)){
+    if (!fs.existsSync(outputDir)) {
         fs.mkdirSync(outputDir, { recursive: true });
     }
 
@@ -52,13 +52,47 @@ async function sliceImageAndExtractText(imagePath, rows, cols, outputDir, custom
     console.log(`Создано и обработано ${rows * cols} изображений в папке "${outputDir}"`);
 
     // Сохраняем результат в JSON-файл
-    fs.writeFileSync(path.join(outputDir, 'result.json'), JSON.stringify(jsonResult, null, 2));
-    console.log(`Результаты сохранены в "result.json"`);
+    const resultFilePath = path.join(outputDir, 'result.json');
+    fs.writeFileSync(resultFilePath, JSON.stringify(jsonResult, null, 2));
+    console.log(`Результаты сохранены в "${resultFilePath}"`);
+
+    // Удаляем все нарезанные изображения
+    await cleanUpImages(outputDir);
 }
 
+async function cleanUpImages(outputDir) {
+    fs.readdir(outputDir, (err, files) => {
+        if (err) {
+            console.error('Ошибка при чтении папки:', err);
+            return;
+        }
+
+        // Фильтруем только JPG файлы и удаляем их
+        const deletePromises = files.map(file => {
+            if (path.extname(file) === '.jpg') {
+                return new Promise((resolve, reject) => {
+                    fs.unlink(path.join(outputDir, file), (err) => {
+                        if (err) {
+                            console.error('Ошибка при удалении файла:', err);
+                            reject(err); // Обработка ошибок
+                        } else {
+                            console.log(`Удален файл: ${file}`);
+                            resolve();
+                        }
+                    });
+                });
+            }
+        });
+
+        // Ждем, пока все файлы будут удалены
+        return Promise.all(deletePromises);
+    });
+}
+
+// Пример использования
 (async () => {
     const imagePath = await loadImagePath();
-    const outputDir = __dirname + '/image/result';
+    const outputDir = path.join(__dirname, 'image', 'result');
     const rows = 1; // Задай нужное количество строк
     const cols = 13; // Задай нужное количество столбцов
 
@@ -66,4 +100,3 @@ async function sliceImageAndExtractText(imagePath, rows, cols, outputDir, custom
 
     await sliceImageAndExtractText(imagePath, rows, cols, outputDir, customNames);
 })();
-
